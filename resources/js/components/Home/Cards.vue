bd4d933656c9749e68214f51c9b99bf8d388c65a
kapureka6174 <mitsu.n.0109@gmail.com>
2021-09-30 04:33:49 +0900

    画像ファイルのパスを本番環境へ変更

--- resources/js/components/Home/Cards.vue
+++ resources/js/components/Home/Cards.vue
@@ -35,21 +35,21 @@ export default {
                 content.description =
                     "紹介文、得意な言語、プログラミング歴などのプロフィールを設定してみましょう。";
                 content.image =
-                    "http://localhost:8000/storage/images/プロフィール.png";
+                    "https://web-service-hub.herokuapp.com/storage/images/プロフィール.png";
                 break;
             case 2:
                 content.title = "サービス登録";
                 content.description =
                     "作ってみたいサービス、作成中のサービス、公開済みのサービスを技術や設計も含めて登録してみよう。";
                 content.image =
-                    "http://localhost:8000/storage/images/サービス登録.png";
+                    "https://web-service-hub.herokuapp.com/storage/images/サービス登録.png";
                 break;
             case 3:
                 content.title = "質問・アドバイス";
                 content.description =
                     "開発中のサービスにはアドバイスや技術選定や設計に関する質問をしてみよう。";
                 content.image =
-                    "http://localhost:8000/storage/images/コメント.png";
+                    "https://web-service-hub.herokuapp.com/storage/images/コメント.png";
                 break;
             default:
                 break;
