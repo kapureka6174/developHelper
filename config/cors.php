abaa94823ad76fa2b0b60e3455198258a8bf6685
kapureka6174 <mitsu.n.0109@gmail.com>
2021-09-30 10:16:10 +0900

    corsの設定の見直し

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://web-service-hub.herokuapp.com'],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
