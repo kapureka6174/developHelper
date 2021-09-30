0100d5ffae603674e9aefd5eb75c96c97157540a
kapureka6174 <mitsu.n.0109@gmail.com>
2021-09-30 21:59:22 +0900

    axiosのheaderにAccess-Control-Allow-Originを追加

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -9,6 +9,7 @@ window._ = require("lodash");
 window.axios = require("axios");
 
 window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
+window.axios.defaults.headers.common["Access-Control-Allow-Origin"] = "*";
 
 window.axios.defaults.withCredentials = true;
 
