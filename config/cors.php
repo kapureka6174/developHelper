fc382aa3ae4284528161bb26fc38e44c6fe7ff07
kapureka6174 <mitsu.n.0109@gmail.com>
2021-09-30 21:12:31 +0900

    corsに直接URLを追加

--- config/cors.php
+++ config/cors.php
@@ -21,9 +21,9 @@ return [
 
     'allowed_origins' => ['https://web-service-hub.herokuapp.com'],
 
-    'allowed_origins_patterns' => ['auth', 'github.com', 'accounts.google.com'],
+    'allowed_origins_patterns' => ['https://accounts.google.com/o/oauth2/auth?client_id=828325844965-t7lk863sqvq48e30ilqammosksdg1jdt.apps.googleusercontent.com&scope=openid+profile+email&response_type=code&state=lGLweutG4McqiHwWdthrijlkxKH1gfIez6BDwzzK','https://github.com/login/oauth/authorize?client_id=8bec41070201a7c21172&redirect_uri=https%3A%2F%2Fweb-service-hub.herokuapp.com%2Fauth%2Fgithub%2Fcallback&scope=user%3Aemail&response_type=code&state=8nUOqzwsyYzFoR0wEfzfTGhKGOJzoqmlh9WWkk2Z'],
 
-    'allowed_headers' => ['*'],
+    'allowed_headers' => ['Access-Control-Allow-Origin'],
 
     'exposed_headers' => false,
 
