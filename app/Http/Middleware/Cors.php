222e2504f036b7d5bc65e26836b46709754881ad
kapureka6174 <mitsu.n.0109@gmail.com>
2021-09-30 09:50:15 +0900

    corsの範囲を拡大

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -8,9 +8,14 @@ class Cors
 {
     public function handle($request, Closure $next)
     {
-        return $next($request)
-            ->header('Access-Control-Allow-Origin', '*')
-            ->header('Access-Control-Allow-Methods', 'GET, POST')
-            ->header('Access-Control-Allow-Headers', 'Accept, X-Requested-With, Origin, Content-Type');
+        // すべてのレスポンスに CORS 用のヘッダーを追加する必要はないので URL から判断する
+        $paths = explode('/', \Request::getPathInfo());
+        if ($paths[1] === 'auth') {
+            return $next($request)
+                ->header('Access-Control-Allow-Origin', '*')
+                ->header('Access-Control-Allow-Methods', 'GET, POST')
+                ->header('Access-Control-Allow-Headers', 'Accept, X-Requested-With, Origin, Content-Type');
+        }
+        return $next($request);
     }
 }
